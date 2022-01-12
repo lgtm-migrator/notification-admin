920bc05ab43f4070992f520e8c937ab4cac38e07
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-01-12 16:17:32 -0500

    Make cache configurable by environment vars

--- app/extensions.py
+++ app/extensions.py
@@ -3,9 +3,15 @@ from notifications_utils.clients.antivirus.antivirus_client import AntivirusClie
 from notifications_utils.clients.redis.redis_client import RedisClient
 from notifications_utils.clients.statsd.statsd_client import StatsdClient
 from notifications_utils.clients.zendesk.zendesk_client import ZendeskClient
+import os
 
 antivirus_client = AntivirusClient()
 statsd_client = StatsdClient()
 zendesk_client = ZendeskClient()
 redis_client = RedisClient()
-cache = Cache(config={"CACHE_TYPE": "simple"})  # TODO: pull config out to config.py later
+
+cache = Cache(config={
+    "CACHE_TYPE": os.environ.get("FLASK_CACHE_TYPE", "SimpleCache"),
+    "CACHE_DEFAULT_TIMEOUT": os.environ.get("FLASK_CACHE_DEFAULT_TIMEOUT", 300),
+    "CACHE_REDIS_HOST": os.environ.get("FLASK_CACHE_REDIS_HOST", "redis") # ignored if SimpleCache
+})
